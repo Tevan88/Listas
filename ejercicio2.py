14dfc9ce727cae25d5d3ce81a60f4e9d9b222520
Tevan88 <tevanrivarola@gmail.com>
2025-04-26 13:51:18 -0300

    Listo el 2

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+#Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
+#penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
+#indexing con números negativos!
+
+lista = ["Esteban", 9.5, False, "Omar", 8]
+print(lista[3]) 
+print(lista[-2]) #Mismo resultado itilizando el indexing con números negativos para recorrer la lista del final hacia el inicio.
