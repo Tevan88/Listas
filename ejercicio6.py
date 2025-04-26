d50efc387e78767055177a3837f43bf628719395
Tevan88 <tevanrivarola@gmail.com>
2025-04-26 14:07:35 -0300

    Listo el 6

---
+++ ejercicio6.py
@@ -0,0 +1,6 @@
+#Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
+#pantalla los dos primeros.
+
+lista_numeros = list(range(10, 31, 5))
+print(lista_numeros[0])
+print(lista_numeros[1])
