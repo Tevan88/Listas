8de5e33d97087f3f6affe156fd9d3708efa48919
Tevan88 <tevanrivarola@gmail.com>
2025-04-26 14:10:05 -0300

    Listo el 7

---
+++ ejercicio7.py
@@ -0,0 +1,8 @@
+#Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores
+#cualesquiera.
+
+autos = ["sedan", "polo", "suran", "gol"]
+print(autos)
+autos[1] = "ecosport"
+autos[2] = "uno"
+print(autos)
