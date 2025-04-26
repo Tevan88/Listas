fd196c4ef20476c4e17da2965efedb727537f006
Tevan88 <tevanrivarola@gmail.com>
2025-04-26 13:59:31 -0300

    Listo el 4

---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+#Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,
+#respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra
+#en los videos o bien investigar cómo funciona el indexing con números negativos!
+
+animales = ["perro", "gato", "conejo", "pez"]
+print(animales)
+animales[1] = "loro"
+animales[3] = "oso"
+print(animales)
