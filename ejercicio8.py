495a24c97ccd49e23125aecb58f090ad835dab5c
Tevan88 <tevanrivarola@gmail.com>
2025-05-06 10:03:24 -0300

    Listo el 8

---
+++ ejercicio8.py
@@ -0,0 +1,8 @@
+#Creamos una lista vacía.
+dobles = []
+#Agregamos con append línea por línea el doble de 5, de 10 y de 15.
+dobles.append((5*2))
+dobles.append((10*2))
+dobles.append((15*2))
+#Mostramos por pantalla la lista completa.
+print(dobles)
